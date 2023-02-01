d82c39bb7264ea729b35c659f8b8047c35cf51c8
cjaneth <cjaneth@unicauca.edu.co>
2023-02-01 16:37:40 -0500

    creando proyecto

---
+++ API_RES_SUBASTA_PRODUCTOS/api_rest_subasta_productos/src/main/java/co/edu/unicauca/distribuidos/api_rest_subasta_productos/ApiRestSubastaProductosApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unicauca.distribuidos.api_rest_subasta_productos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestSubastaProductosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestSubastaProductosApplication.class, args);
+	}
+
+}
---
+++ API_RES_SUBASTA_PRODUCTOS/api_rest_subasta_productos/src/test/java/co/edu/unicauca/distribuidos/api_rest_subasta_productos/ApiRestSubastaProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unicauca.distribuidos.api_rest_subasta_productos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestSubastaProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
